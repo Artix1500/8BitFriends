e799ce442300261c17d37b9a1d40b280c4798b09
Artix1500 <artur.haczek23@gmail.com>
2020-02-28 16:24:08 +1100

    Add searchBar to Discover view

--- views/Discover.js
+++ views/Discover.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet} from 'react-native';
 import Header from '../components/Header';
+import {SearchBar} from 'react-native-elements';
 
 const Discover = ({navigation}) => {
+  let [search, setSearch] = useState('');
   return (
     <View style={styles.container}>
       <Header title="8BitFriends" logout={true} navigation={navigation} />
+
+      <SearchBar
+        lightTheme={true}
+        placeholder="Type Here..."
+        onChangeText={value => setSearch(value)}
+        value={search}
+      />
     </View>
   );
 };
