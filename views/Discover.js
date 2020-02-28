184ca0af6c6d3ee4e6256d0bef3f223d7807e587
Artix1500 <artur.haczek23@gmail.com>
2020-02-28 18:54:05 +1100

    Add query to QraphQL in Discover view

--- views/Discover.js
+++ views/Discover.js
@@ -1,10 +1,45 @@
 import React, {useState} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
 import Header from '../components/Header';
 import {SearchBar} from 'react-native-elements';
+import axios from 'axios';
 
 const Discover = ({navigation}) => {
   let [search, setSearch] = useState('');
+  let [data, setData] = useState([]);
+
+  const handleSearch = async value => {
+    setSearch(value);
+
+    // search
+    try {
+      let res = await axios.post(
+        'https://api.github.com/graphql',
+        {
+          query: `query { 
+            search(first: 10, query: "${value}", type: USER){
+              userCount
+              nodes{
+                __typename
+                ... on User {
+                  name
+                  login
+                }
+              }
+            } 
+          }`,
+        },
+        {
+          headers: {
+            Authorization: 'bearer 388067e911d6699f259e055b71824dd01e0157f0',
+          },
+        },
+      );
+      setData(res.data.data.search.nodes);
+    } catch (error) {
+      console.log('ERROR');
+    }
+  };
   return (
     <View style={styles.container}>
       <Header title="8BitFriends" logout={true} navigation={navigation} />
@@ -12,9 +47,11 @@ const Discover = ({navigation}) => {
       <SearchBar
         lightTheme={true}
         placeholder="Type Here..."
-        onChangeText={value => setSearch(value)}
+        onChangeText={handleSearch}
         value={search}
       />
+
+      <Text>debug search: {search}</Text>
     </View>
   );
 };
